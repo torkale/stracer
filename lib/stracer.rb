c7111ff6093e645eedf03e6bac06f035ef34e044
tor <torkale@gmail.com>
2013-10-14 11:47:32 +0300

    config

--- lib/stracer.rb
+++ lib/stracer.rb
@@ -7,10 +7,10 @@ require 'log4r/yamlconfigurator'
 module Stracer
   def self.recorder(opts)
     env = opts[:environment].to_s
-     stats = Statsd.new(opts[:statsd_host], 8125).tap do
+     stats = Statsd.new(opts[:statsd_host], opts[:statsd_port]).tap do
        |st| st.namespace = "#{env}.#{opts[:service_name]}"
      end
-    Log4r::YamlConfigurator.load_yaml_file(opts[:log4r_config])
+    Log4r::YamlConfigurator.load_yaml_string(opts[:log4r_config])
     log = Log4r::Logger[env]
     Recorder.new(stats, log, host: Socket.gethostname.gsub(/\./,'_'))
   end
